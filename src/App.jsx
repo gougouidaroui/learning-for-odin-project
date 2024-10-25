11d02e86074c2171cbfe03aa3e2afe9b82023bbd
yoonhaw <bbabrehamza@gmail.com>
2024-10-25 19:19:10 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <header>
+                <ul>
+                    <li><a href=""></a>something</li>
+                    <li><a href=""></a>something</li>
+                    <li><a href=""></a>something</li>
+                    <li><a href=""></a>something</li>
+                </ul>
+            </header>
+            <main>
+                <section id="product">
+                </section>
+            </main>
+
+        </>
+    )
+}
+
+export default App
